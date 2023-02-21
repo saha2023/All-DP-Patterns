8cf5ace45ec1ce3213bd9fea5afc46a037ba7cf4
arko <arkajyotisaha891@gmail.com>
2023-02-21 16:19:11 +0530

    maxLenPairChain

---
+++ Linear DP/maxLengthOfPairOfChain.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findLongestChain(vector<vector<int>>& pairs) {
+        auto cmp = [&](vector<int>& a, vector<int>&b){
+            return a[1] < b[1] || a[1] == b[1] && a[0] < b[0];
+        };
+        sort(pairs.begin(), pairs.end(), cmp);
+        int cnt = 0;
+        vector<int>& pair = pairs[0];
+        for (int i = 0; i < pairs.size(); i++) {
+            if (i == 0 || pairs[i][0] > pair[1]) {
+                pair = pairs[i];
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+};
